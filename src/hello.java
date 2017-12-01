a492daa0103ba742667bcc6b67ae6092e46c6e3f
JJ Keeney <jjkeeney129285@gmail.com>
2017-12-01 09:59:58 -0700

    printed five

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,7 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello JJ! How are you?");
+        int num = 5;
+        System.out.println(num);
     }
 }
