786db016144bb9f8d489feab2728c6d73aafbd4b
JJ Keeney <jjkeeney129285@gmail.com>
2017-12-01 10:10:43 -0700

    playing with ints and adding and subtracting

--- src/hello.java
+++ src/hello.java
@@ -2,6 +2,10 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello JJ! How are you?");
         int num = 5;
-        System.out.println(num);
+        int num2 = 200;
+        int num3 = -89;
+        int tot = num + num2 + num3;
+        int diff = 1000 - tot;
+        System.out.println(diff);
     }
 }
